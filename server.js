30c4016f35678606d7e20f34b5f5f9c50d1bae1d
Ajeywatpade <ajey.archent@gmail.com>
2025-06-11 12:40:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,25 +1,14 @@
 'use strict'
 
-/*
-|--------------------------------------------------------------------------
-| Http server
-|--------------------------------------------------------------------------
-|
-| This file bootstraps Adonisjs to start the HTTP server. You are free to
-| customize the process of booting the http server.
-|
-| """ Loading ace commands """
-|     At times you may want to load ace commands when starting the HTTP server.
-|     Same can be done by chaining `loadCommands()` method after
-|
-| """ Preloading files """
-|     Also you can preload files by calling `preLoad('path/to/file')` method.
-|     Make sure to pass a relative path from the project root.
-*/
-
 const { Ignitor } = require('@adonisjs/ignitor')
 
 new Ignitor(require('@adonisjs/fold'))
   .appRoot(__dirname)
-  .fireHttpServer()
+  .fireHttpServer((handler) => {
+    const { Server } = require('@adonisjs/http-server')
+    return new Server(handler).listen(
+      process.env.PORT || 3333,
+      process.env.HOST || '0.0.0.0'
+    )
+  })
   .catch(console.error)
