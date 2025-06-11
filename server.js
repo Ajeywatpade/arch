c7cba6dabb88e0fd9f6ebe61d5bf14ff66b57e9b
Ajeywatpade <ajey.archent@gmail.com>
2025-06-11 12:42:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,11 +4,5 @@ const { Ignitor } = require('@adonisjs/ignitor')
 
 new Ignitor(require('@adonisjs/fold'))
   .appRoot(__dirname)
-  .fireHttpServer((handler) => {
-    const { Server } = require('@adonisjs/http-server')
-    return new Server(handler).listen(
-      process.env.PORT || 3333,
-      process.env.HOST || '0.0.0.0'
-    )
-  })
+  .fireHttpServer()
   .catch(console.error)
